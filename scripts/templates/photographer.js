b0cd3c201e0d60291813b238f2feb315c6c7195e
Rochet brice <brice.974452@gmail.com>
2023-08-04 11:36:59 +0200

    Ajout du css sur la page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -6,21 +6,31 @@ function photographerTemplate(data) {
     function getUserCardDOM() {
         const article = document.createElement('article');
         const img = document.createElement('img');
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute('alt', 'Photo portrait de ' + name);
+
         const h2 = document.createElement('h2');
         h2.textContent = name;
+
         const pLocation = document.createElement('p');
         pLocation.textContent = city + ', ' + country;
+        pLocation.setAttribute('class', 'pLocation');
+
         const pTagline = document.createElement('p');
         pTagline.textContent = tagline;
+        pTagline.setAttribute('class', 'pTag');
+
         const pPrice = document.createElement('p');
-        pPrice.textContent = price;
+        pPrice.textContent = price + "€/jour";
+        pPrice.setAttribute('class', 'pPrice');
+
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(pLocation);
         article.appendChild(pTagline);
         article.appendChild(pPrice);
+        // ajouter un attr aria-label sur les liens quand je les mettraient
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
