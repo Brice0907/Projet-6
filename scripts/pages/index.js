b6d8306677cd464628a54165d8e57f332fe6f707
Rochet brice <brice.974452@gmail.com>
2023-08-04 09:39:53 +0200

    On  récupère les données Json

---
+++ scripts/pages/index.js
@@ -0,0 +1,25 @@
+async function getPhotographers() {
+    const response = await fetch('.././data/photographers.json');
+    const datas = await response.json();
+    console.log(datas);
+    return datas;
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
+
