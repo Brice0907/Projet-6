16c8e60a30975cbea59cceb09a950318c6051b3c
Rochet brice <brice.974452@gmail.com>
2023-08-04 10:22:35 +0200

    Affichage des données json

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,25 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        const pLocation = document.createElement('p');
+        pLocation.textContent = city + ', ' + country;
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+        const pPrice = document.createElement('p');
+        pPrice.textContent = price;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pLocation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
